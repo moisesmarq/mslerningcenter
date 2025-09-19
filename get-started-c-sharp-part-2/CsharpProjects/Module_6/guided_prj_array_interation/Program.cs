0ea4d11f3347e441a92675ad31dedc730d423b01
moisesmarq <moises.marques@outlook.com>
2025-09-19 16:24:52 -0300

    Projeto Guiado - Adiciona cálculo e exibição das notas dos alunos com suas respectivas letras

--- get-started-c-sharp-part-2/CsharpProjects/Module_6/guided_prj_array_interation/Program.cs
+++ get-started-c-sharp-part-2/CsharpProjects/Module_6/guided_prj_array_interation/Program.cs
@@ -21,6 +21,9 @@ int[] loganScores = [90, 95, 87, 88, 96];
 //Array que vai receber os array de notas do estudantes
 int[] studentScores = new int[10];
 
+string currentStudentLetterGrade = "";
+
+
 Console.WriteLine("Student\t\tGrade\n");
 
 foreach (string name in studentNames)
@@ -55,7 +58,46 @@ foreach (string name in studentNames)
 
     currentStudentGrade = (decimal)scoreSum / currentAssignments;
 
-    Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t?");
+    if (currentStudentGrade >= 97)
+        currentStudentLetterGrade = "A+";
+
+    else if (currentStudentGrade >= 93)
+        currentStudentLetterGrade = "A";
+
+    else if (currentStudentGrade >= 90)
+        currentStudentLetterGrade = "A-";
+
+    else if (currentStudentGrade >= 87)
+        currentStudentLetterGrade = "B+";
+
+    else if (currentStudentGrade >= 83)
+        currentStudentLetterGrade = "B";
+
+    else if (currentStudentGrade >= 80)
+        currentStudentLetterGrade = "B-";
+
+    else if (currentStudentGrade >= 77)
+        currentStudentLetterGrade = "C+";
+
+    else if (currentStudentGrade >= 73)
+        currentStudentLetterGrade = "C";
+
+    else if (currentStudentGrade >= 70)
+        currentStudentLetterGrade = "C-";
+
+    else if (currentStudentGrade >= 67)
+        currentStudentLetterGrade = "D+";
+
+    else if (currentStudentGrade >= 63)
+        currentStudentLetterGrade = "D";
+
+    else if (currentStudentGrade >= 60)
+        currentStudentLetterGrade = "D-";
+    
+    else
+        currentStudentLetterGrade = "F";
+
+    Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t{currentStudentLetterGrade}");
 }
 
 Console.WriteLine("Press the Enter key to continue");
