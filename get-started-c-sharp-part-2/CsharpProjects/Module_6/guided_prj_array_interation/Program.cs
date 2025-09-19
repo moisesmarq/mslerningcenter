b805ceb6002ef107bb716434653331ff0ab67cff
moisesmarq <moises.marques@outlook.com>
2025-09-19 16:14:28 -0300

    Projeto Guiado - Refatoração do tratamento da pontuação dos alunos para maior clareza e organização

--- get-started-c-sharp-part-2/CsharpProjects/Module_6/guided_prj_array_interation/Program.cs
+++ get-started-c-sharp-part-2/CsharpProjects/Module_6/guided_prj_array_interation/Program.cs
@@ -12,31 +12,50 @@ string[] studentNames = new string[]
     "Logan"
 };
 
-
 //Declaracao do arrays que receberam as notas.
 int[] sophiaScores = new int[] { 90, 86, 87, 98, 100 };
 int[] andrewScores = [92, 89, 81, 96, 90];
 int[] emmaScores = new int[] { 90, 85, 87, 98, 68 };
 int[] loganScores = [90, 95, 87, 88, 96];
 
-int sophiaSum = 0;
-decimal sophiaScore;
+//Array que vai receber os array de notas do estudantes
+int[] studentScores = new int[10];
+
+Console.WriteLine("Student\t\tGrade\n");
 
 foreach (string name in studentNames)
 {
-    if (name == "Sophia")
+    string currentStudent = name;
+
+    if (currentStudent == "Sophia")
     {
-        foreach (int score in sophiaScores)
-        {
-            // add the exam score to the sum
-            sophiaSum += score;
-        }
-
-        sophiaScore = (decimal)sophiaSum / currentAssignments;
-
-        Console.WriteLine("Student\t\tGrade\n");
-        Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
+        studentScores = sophiaScores;
     }
+    else if (currentStudent == "Andrew")
+    {
+        studentScores = andrewScores;
+    }
+    else if (currentStudent == "Emma")
+    {
+        studentScores = emmaScores;
+    }
+    else if (currentStudent == "Logan")
+    {
+        studentScores = loganScores;
+    }
+
+    int scoreSum = 0;
+    decimal currentStudentGrade = 0;
+
+    foreach (int score in studentScores)
+    {
+        // add the exam score to the sum
+        scoreSum += score;
+    }
+
+    currentStudentGrade = (decimal)scoreSum / currentAssignments;
+
+    Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t?");
 }
 
 Console.WriteLine("Press the Enter key to continue");
